afbb7d824dc774a4d12c93005e4685d9b28dbc14
Nishand-RM <nishandrm9@gmail.com>
2024-08-01 23:18:02 +0530

    creating component inside main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+function Mob(){
+  return   <h1>Hello Nishand RM!</h1>
+
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
- <h1>Hello Nishand RM!</h1>
+
+  <Mob />
 )
