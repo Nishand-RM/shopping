20f49e68423f6f73d2382b9019f220b16c33567e
Nishand-RM <nishandrm9@gmail.com>
2024-08-02 00:14:03 +0530

    creating a component in function in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function Mob(){
+    return   (
+    <div>
+    <h1>Hello Nishand RM!</h1>
+    <p>How r u?</p>
+    </div>
+    )
+  
+  }
+
+  export default Mob;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Mob from './App'
 
-function Mob(){
-  return   <h1>Hello Nishand RM!</h1>
-
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <Mob />
+ <Mob />
 )
