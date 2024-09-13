1c622762578d0036c2bf1f210f6cf28968dc35f5
Nishand-RM <nishandrm9@gmail.com>
2024-09-13 22:33:30 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['bootstrap/dist/css/bootstrap.min.css']
+    },
+    outDir: 'dist',  // Ensure the output goes to 'dist'
+  },
 });
